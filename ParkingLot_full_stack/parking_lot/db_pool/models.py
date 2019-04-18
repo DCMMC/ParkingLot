2e926e95b937c6e56e65781cf44add7c0f90db2b
dcmmc <xwt97294597@gmail.com>
2019-04-18 19:09:32 +0800

    TODO

--- ParkingLot_full_stack/parking_lot/db_pool/models.py
+++ ParkingLot_full_stack/parking_lot/db_pool/models.py
@@ -5,17 +5,19 @@ from mongoengine import StringField
 from mongoengine import DateTimeField
 from mongoengine import ReferenceField
 from mongoengine import ListField
+from mongoengine import DecimalField
 from mongoengine import IntField
 from mongoengine import MapField
 from mongoengine import LazyReferenceField
+from mongoengine import BooleanField
 import datetime
 
 
 class Vehicle(Document):
     """
-    TODO
     车辆信息
     """
+    # 车牌为主键
     license_plate = StringField(max_length=20, primary_key=True,
                                 unique=True, required=True)
     # 备注
@@ -41,7 +43,7 @@ class MemberCard(Document):
     card_type = StringField(max_length=20, required=True)
     addition_info = StringField(max_length=10000)
     # 会员卡与车辆双向绑定, 这个是可选的
-    bind_vehicles = ListField(ReferenceField(Vehicle))
+    bind_vehicles = MapField(ReferenceField(Vehicle))
 
 
 class MemberCardLog(Document):
@@ -51,26 +53,30 @@ class MemberCardLog(Document):
     date_log = DateTimeField(default=datetime.datetime.utcnow)
     # 'create', 'update', 'remove'
     event_type = StringField(max_length=20, required=True)
-    # 相关联的会员卡, 如果 event_type 是 'remove'
+    # 相关联的会员卡, 如果 event_type 是 'remove', 则为空
+    # 为了保证安全, 删除操作必须前端重点提醒, 并且将 memberCard 当中的
+    # 信息存在日志的 addition_info 里面
     card_reference = ReferenceField(MemberCard)
-    addition_info = StringField(max_length=10000)
+    addition_info = StringField(max_length=10000, require=True)
     # 产生该日志的操作员信息
     admin_info = StringField(max_length=50, required=True)
 
 
 class Parking(Document):
     """
-    TODO:
     车位信息
     """
+    # 车位 id 推荐用停车场 pk + 车位号(在 models.json 里面的) 的结合
     parking_id = StringField(max_length=20, primary_key=True,
                              unique=True, required=True)
     addition_info = StringField(max_length=10000, default='')
-    phone_number = StringField(max_length=14, default='')
-    location = StringField(max_length=100, default='')
-    # 出入口
-    indoors = ListField(ReferenceField('Door'))
-    outdoors = ListField(ReferenceField('Door'))
+    floor = ReferenceField('Floor', require=True)
+    region = ReferenceField('Region', require=True)
+    # 'normal'(正常使用), 'unavailable'(不可用, e.g., 正在维修),
+    # 'obligated'(特殊情况预留, e.g., 已预约)
+    status = StringField(max_length=20, default='normal')
+    # 是否有车在停车位上
+    used = BooleanField(default=False)
 
 
 class Floor(Document):
@@ -78,14 +84,17 @@ class Floor(Document):
     楼层信息
     """
     name = StringField(max_length=100, required=True)
-    id_in_map = StringField(max_length=100, required=True)
+    # 不作为 pk, 因为如果存在多个停车场, 就看会冲突
+    id_in_map = StringField(max_length=100, required=True, unique=True)
     # 几楼?
     floor_num = IntField(required=True)
     # 楼层里面可能有多个分区
     # 使用懒加载, 避免只需要 pk 的时候的性能开销
     # 访问的时候会返回一个 class instance, 所以要手动
     # fetch() 才能解引用
+    # {id_in_map: LazyRefField} 的字典
     regions = MapField(field=LazyReferenceField('Region'))
+    addition_info = StringField(max_length=10000, default='')
 
 
 class Region(Document):
@@ -94,10 +103,13 @@ class Region(Document):
     """
     name = StringField(max_length=100, required=True)
     # DCMMC: TODO: 这个在模型的 json 里面暂时还没有...
-    id_in_map = StringField(max_length=100, required=True)
-    floor = ReferenceField('Floor')
+    id_in_map = StringField(max_length=100, required=True,
+                            unique=True)
+    floor = ReferenceField('Floor', require=True)
     # 车位
+    # {id_in_map: LazyRefField} 的字典
     parkings = MapField(field=LazyReferenceField('Parking'))
+    addition_info = StringField(max_length=10000, default='')
 
 
 class Door(Document):
@@ -109,23 +121,66 @@ class Door(Document):
     id_in_map = StringField(max_length=100, required=True)
     # 提高可读性, 出入口名称, e.g. 西一门, 最好要跟建模 json 文件对应起来!
     name = StringField(max_length=100, required=True)
+    addition_info = StringField(max_length=10000, default='')
+    # 'indoor', 'outdoor'
+    door_type = StringField(max_length=20, require=True)
+    # 'working', 'closed'
+    status = StringField(max_length=20, require=True)
 
 
 class ParkingLot(Document):
     """
     停车场信息
     """
-    parking_lot_name = StringField(max_length=50, primary_key=True,
-                                   unique=True, required=True)
+    # 停车场名称必须唯一
+    parking_lot_name = StringField(max_length=50, required=True,
+                                   primary_key=True, uniqu=True)
     addition_info = StringField(max_length=10000, default='')
+    # 联系电话
+    phone_number = StringField(max_length=14, default='')
+    location = StringField(max_length=100, default='')
+    # 出入口
+    # 这三个 map 都是 {id_in_map: RefField}
+    indoors = MapField(ReferenceField('Door'))
+    outdoors = MapField(ReferenceField('Door'))
+    floors = MapField(ReferenceField(Floor))
 
 
-class Bill(Document):
+class ParkingLotLog(Document):
+    """
+    停车场信息维护
+    主要是更新, 创建和删除一般不推荐做
+    以为创建和删除之后最好还是要跟 models.json 里面的信息
+    保持一致.
+    Floor, Rigon, Parking, ParkingLot 这些数据的创建
+    都是靠直接从 models.json 导入
+    """
+    date_created = DateTimeField(default=datetime.datetime.utcnow)
+    admin_info = StringField(max_length=50, required=True)
+    event_type = StringField(max_length=100, required=True)
+    # 具体操作信息用 String 来记录
+    event_info = StringField(max_length=10000, required=True)
+
+
+class BillLog(Document):
     """
     账单信息
     Time series
     """
-    date_created = DateTimeField(default=datetime.datetime.utcnow)
+    date_in = DateTimeField(required=True)
+    date_out = DateTimeField(required=True)
+    addition_info = StringField(max_length=10000, default='')
+    # 暂时没法直到具体车位, 所以暂时设置为 optional
+    parking_info = ReferenceField(Parking)
+    # 懒加载, 到时候我们要根据 pk 搜索日志, 所以必须要用这个为了效率
+    vehicle = LazyReferenceField(Vehicle, required=True)
+    # 非会员用户或者储值卡(top-up)用户
+    fee = DecimalField(min_value=0., precision=3, require=True)
+    # 如果使用了(储值卡/记次卡)才需要用到 member_card
+    member_card = LazyReferenceField(MemberCard)
+    indoor = LazyReferenceField(Door, require=True)
+    outdoor = LazyReferenceField(Door, require=True)
+    admin_info = StringField(max_length=50, required=True)
 
 
 # class Coach(Document):
--- ParkingLot_full_stack/parking_lot/db_pool/operations.py
+++ ParkingLot_full_stack/parking_lot/db_pool/operations.py
@@ -1,1 +1,109 @@
 # 一堆对 models 的数据库操作的封装
+
+
+def load_model_create_db(file_abspath):
+    """
+    从停车场建模的 json 文件中导入停车场数据并且创建对应的数据库
+    @param file_abspath 模型文件的绝对路径
+    """
+    pass
+
+
+def updateFloor():
+    pass
+
+
+def updateRegion():
+    pass
+
+
+def updateParking():
+    pass
+
+
+def updateParkingLot():
+    pass
+
+
+def addMemberCard():
+    pass
+
+
+def updateMemberCard():
+    pass
+
+
+def removeMemberCard():
+    pass
+
+
+def vehicle_enter():
+    pass
+
+
+def vehicle_leave():
+    pass
+
+
+def updateDoor():
+    pass
+
+
+def getBillsByDateRangeCheckin():
+    pass
+
+
+def getBillsByDateRangeCheckout():
+    pass
+
+
+def getBillsByLicense():
+    pass
+
+
+def getBillsByAdmin():
+    pass
+
+
+def getBillsByType():
+    pass
+
+
+def getMemberCardLogsByDateRange():
+    pass
+
+
+def getMemberCardLogByCardId():
+    pass
+
+
+def getMemberCardsLogsByLicense():
+    pass
+
+
+def getMemberCardLogsByAdmin():
+    pass
+
+
+def getMemberCardLogsByType():
+    pass
+
+
+def getLicensesByPhone():
+    pass
+
+
+def getLiensesByOwnName():
+    pass
+
+
+def getParkingLogsByDateRange():
+    pass
+
+
+def getParkingLotLogsByType():
+    pass
+
+
+def getParkingLotLogsByAdmin():
+    pass
